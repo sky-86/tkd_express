44be9570ca53aa496fb6bea36618ca8edd1d23ea
sky-86 <sky86one@gmail.com>
2022-12-29 13:12:22 -0600

    finished setup and backend

---
+++ src/components/calendar.tsx
@@ -0,0 +1,20 @@
+import './calendar.css';
+
+// https://support.google.com/calendar/answer/41207?hl=en
+// google calender
+function Calendar() {
+  return (
+    <>
+      <iframe 
+        title='google-calender'
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FChicago&src=ODk4ZDk3MWM5OTkwNTQ4MDU2YzdhYzJkNWQ4MTM4NTdkMmRhMGNlNGQwMjRlNzA1ZmQ2MjVjN2IwZjBkYjlmMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%237CB342" 
+        width="800" 
+        height="600" 
+        //frameBorder="0" 
+        //scrolling="no"
+      ></iframe>
+    </>
+  )
+}
+
+export default Calendar;
